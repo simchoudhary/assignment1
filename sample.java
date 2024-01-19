a843b7bc06c69195684fe4051a16bddb2c3e0991
simchoudhary <144848673+simchoudhary@users.noreply.github.com>
2024-01-18 22:05:57 -0500

    Add files via upload
    assignment 1

---
+++ sample.java
@@ -0,0 +1,11 @@
+package edu.neu.csye6200;
+
+public class sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("Hello, World!");
+
+	}
+
+}
