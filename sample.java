f10973a4410129a289a367c00c20a9b8e84ce0b3
simchoudhary <144848673+simchoudhary@users.noreply.github.com>
2024-01-19 16:52:46 -0500

    update

--- sample.java
+++ sample.java
@@ -4,8 +4,8 @@ public class sample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        System.out.println("Hello, World!");
-hiiii
+        System.out.println("Hello, World");
+
 	}
 
 }
